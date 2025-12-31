348712e08b799e1699d0f7c4bca412430c4a797c
Niko <nikojlarin22@gmail.com>
2025-12-30 20:04:35 -0500

    Enhance alpaca_tools with new stock and account functions
    Added functions for open positions, stock prices, and account value retrieval.

--- Library/alpaca_tools.py
+++ Library/alpaca_tools.py
@@ -1,32 +1,53 @@
 import requests
 import os
+import time
+from datetime import date
+from dateutil.relativedelta import relativedelta
 from dotenv import load_dotenv
 
 load_dotenv()
-
 BASE_URL = "https://paper-api.alpaca.markets/v2"
-
 def headers():
     return {
         "accept": "application/json",
-        "APCA-API-KEY-ID": 'YOUR API KEY',
-        "APCA-API-SECRET-KEY": 'YOUR API KEY',
+        "content-type": "application/json",
+        "APCA-API-KEY-ID": 'YOUR-API-KEY',
+        "APCA-API-SECRET-KEY": 'YOUR-SECRET-API-KEY',
     }
 
+#----------ACCOUNT----------#
 def accountValue():
     response = requests.get(f'{BASE_URL}/account', headers=headers()) # calls headers
     response = response.json()
     
     return response['portfolio_value']
 
-def stockPrice(sym):
+def get_open_positions(): # return all open positions
+    response = requests.get(f'{BASE_URL}/positions', headers=headers()) # calls headers
+    response = response.json()
+    
+    return response['positions'] # all open positions
+
+def position_history():
+    pass
+
+#----------STOCK----------#
+def current_stock_price(sym):
     url = f"https://data.alpaca.markets/v2/stocks/{sym}/snapshot"
 
     response = requests.get(url, headers=headers()) #calls headers function
     response = response.json()
 
     return response['dailyBar']['c']
-        
+
+def open_stock_price(sym):
+    url = f"https://data.alpaca.markets/v2/stocks/{sym}/snapshot"
+
+    response = requests.get(url, headers=headers()) #calls headers function
+    response = response.json()
+
+    return response['dailyBar']['o']
+
 def pctDailyChange(sym):
     url = f"https://data.alpaca.markets/v2/stocks/{sym}/snapshot"
 
@@ -39,41 +60,68 @@ def pctDailyChange(sym):
     pctChange = round(((closePrice - openPrice) / openPrice), 4) * 100
 
     return pctChange
-
-def get_open_positions(): # return all open positions
-    response = requests.get(f'{BASE_URL}/positions', headers=headers()) # calls headers
-    response = response.json()
     
-    return response['positions'] # all open positions
-
 def is_market_open():
     response = requests.get(f'{BASE_URL}/clock', headers=headers()) # calls headers
     response = response.json()
 
     return response['is_open'] #returns bool if mkt open/closed
 
-def option(ticker, oCode): #needs symbol and option code
-    response = requests.get(f"https://data.alpaca.markets/v1beta1/options/snapshots/{ticker}?feed=opra&limit=100&root_symbol={oCode}", headers=headers()) # gets live option based on oCode
+
+#----------OPTIONS----------#
+def option(oCode): #needs symbol and option code
+    url = f"https://data.alpaca.markets/v1beta1/options/snapshots?symbols={oCode}&feed=indicative&limit=1"
+
+    
+    response = requests.get(url, headers=headers()) # gets live option based on oCode
     response = response.json()
 
     snap = response["snapshots"][oCode]
 
-    greeks = response['greeks']
+    code = snap
+    greeks = snap['greeks']
     impliedVolatility = snap['impliedVolatility'] 
     price = snap['latestQuote']['ap']
 
     return (greeks, impliedVolatility, price) # returnts a tuple
 
-def options_chain():
+def options_chain(ticker):
     #implement a simple filter to only get the most recent x strikes
-    response = requests.get(f"https://data.alpaca.markets/v1beta1/options/snapshots/{ticker}")
+    url = f"https://data.alpaca.markets/v1beta1/options/snapshots/{ticker}"
+    
+    response = requests.get(url, headers=headers())
     response = response.json()
+    
+    return response#['snapshots'] # returns huge chain
 
-    return response['snapshots'] # returns huge chain
+#----------INDICATOR----------#
+def aoc(ticker):
+    '''
+    this function pulls open and close data for 100 days and calculates the 
+    average open to close change for one day   
+    '''
+    
+    today = date.today()
+    year_ago = today - relativedelta(years=1)
+    
+    url = f"https://data.alpaca.markets/v2/stocks/bars?symbols={ticker}&timeframe=1D&start={year_ago}&limit=100&adjustment=raw&feed=sip&sort=asc"
 
-def position_history():
-    pass
+    response  = requests.get(url, headers=headers())
+    data = response.json()
 
-def chart():
-    pass
+    bars = data["bars"][ticker]
+
+    abs_pct_changes = []
+
+    for candle in bars:
+        closeD = candle["c"]
+        openD = candle["o"]
+
+        if openD != 0:
+            abs_pct_changes.append(abs(((closeD - openD) / openD)))
+
+    today_aoc = sum(abs_pct_changes) / len(abs_pct_changes)
+
+    return today_aoc / 2
+
 
