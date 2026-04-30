6f2f5985ec16add979f4bca84c310d6c90b5c52d
Niko <nikojlarin22@gmail.com>
2026-04-30 17:28:10 -0400

    Update lagStrategy.py

--- Strategies/lagStrategy.py
+++ Strategies/lagStrategy.py
@@ -4,9 +4,16 @@ def headers():
     return {
         "accept": "application/json",
         "content-type": "application/json",
-        "APCA-API-KEY-ID": '',
-        "APCA-API-SECRET-KEY": '',
+        "APCA-API-KEY-ID": 'x',
+        "APCA-API-SECRET-KEY": 'x',
     }
+
+def all_positions():
+    url = "https://paper-api.alpaca.markets/v2/positions"
+    
+    response = requests.get(url, headers=headers())
+    response = response.json()
+    return response
 
 def get_price_change(sym1,sym2):
     url = f'https://data.alpaca.markets/v2/stocks/bars/latest?symbols={sym1}%2C{sym2}&feed=delayed_sip'
@@ -39,37 +46,47 @@ while True:
         if sym1_pct_change - sym2_pct_change > .2:
             #buy mkt sym2
             #set take profit @ sym1 pct_change * .5 * sym2_close + sym2_close
+            positions = all_positions()
+            if list[1][2] in positions[0].values():
+                continue
             
+            tp = (sym1_pct_change / 100) * .5 * list[1][1] + list[1][1]
             payload = {
             "type": "market",
             "time_in_force": "day",
-            "take_profit": { "limit_price": f'{(sym1_pct_change / 100) * .5 * list[1][1] + list[1][1]}'},
+            "take_profit": { "limit_price": tp},
+            "stop_loss": { "stop_price": "0.99"},
             "symbol": f'{list[1][2]}',
-            "notional": "100",
-            "side": "buy"
+            "side": "buy",
+            "order_class": "bracket",
+            "qty": f'{round(list[1][1] / 1250)}'
             }
         
             response = requests.post(url, json=payload, headers=headers())
         
-            print(response.text)
+            print(f'Buying {list[1][2]} | TP: {tp}')
         
         if sym2_pct_change - sym1_pct_change > .2:
             #buy mkt sym1
             #set take profit @ sym2 pct_change * .5 * sym1_close + sym1_close
+            positions = all_positions()
+            if list[0][2] in positions[0].values():
+                continue
+            
+            tp = (sym2_pct_change / 100) * .5 * list[0][1] + list[0][1]
             payload = {
             "type": "market",
             "time_in_force": "day",
-            "take_profit": { "limit_price": f'{(sym2_pct_change / 100) * .5 * list[0][1] + list[0][1]}'},
+            "take_profit": { "limit_price": tp},
+            "stop_loss": { "stop_price": "0.99" },
             "symbol": f'{list[0][2]}',
-            "notional": "100",
-            "side": "buy"
+            "side": "buy",
+            "order_class": "bracket",
+            "qty": f'{round(list[0][1] / 1250)}'
             }
         
             response = requests.post(url, json=payload, headers=headers())
         
-            print(response.text)
+            print(f'Buying {list[0][2]} | TP: {tp}')
         
-        print(sym1_pct_change, sym2_pct_change, sym2_pct_change / 100 * .5 * list[0][1] + list[0][1], sym1_pct_change / 100 * .5 * list[1][1] + list[1][1])
-
-                
-        
+        print(f'{list[1][2]}/{list[0][2]}')
