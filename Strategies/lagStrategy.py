d57a7b49c60c9a9bb196a23d7cfab0b4b227d4b9
Niko <nikojlarin22@gmail.com>
2026-04-30 09:50:06 -0400

    Update lagStrategy.py

--- Strategies/lagStrategy.py
+++ Strategies/lagStrategy.py
@@ -27,43 +27,48 @@ def get_price_change(sym1,sym2):
 
 url = "https://paper-api.alpaca.markets/v2/orders"
 
-list = get_price_change('NVDA','AMD')
 
-sym1_pct_change = ((list[0][1] + (list[0][0])) / list[0][1] -1) * 100
-sym2_pct_change = ((list[1][1] + (list[1][0])) / list[1][1] -1) * 100
+syms = [[NVDA, AMD], [GLD,SLV], [SPY, QQQ]]
+while True:
+    for i in syms:
+        list = get_price_change(i[0], i[1])
 
-
-if sym1_pct_change - sym2_pct_change > .4:
-    #buy mkt sym2
-    #set take profit @ sym1 pct_change * .5 * sym2_close + sym2_close
-    
-    payload = {
-    "type": "market",
-    "time_in_force": "day",
-    "take_profit": { "limit_price": f'{sym1_pct_change * .5 * list[1][1] + list[1][1]}'},
-    "symbol": f'{list[1][2]}',
-    "notional": "100",
-    "side": "buy"
-    }
-
-    response = requests.post(url, json=payload, headers=headers())
-
-    print(response.text)
-
-if sym2_pct_change - sym1_pct_change > .4:
-    #buy mkt sym1
-    #set take profit @ sym2 pct_change * .5 * sym1_close + sym1_close
-    payload = {
-    "type": "market",
-    "time_in_force": "day",
-    "take_profit": { "limit_price": f'{sym2_pct_change * .5 * list[0][1] + list[0][1]}'},
-    "symbol": f'{list[0][2]}',
-    "notional": "100",
-    "side": "buy"
-    }
-
-    response = requests.post(url, json=payload, headers=headers())
-
-    print(response.text)
-
-print(sym1_pct_change, sym2_pct_change)
+        sym1_pct_change = ((list[0][1] + (list[0][0])) / list[0][1] -1) * 100
+        sym2_pct_change = ((list[1][1] + (list[1][0])) / list[1][1] -1) * 100
+        
+        
+        if sym1_pct_change - sym2_pct_change > .4:
+            #buy mkt sym2
+            #set take profit @ sym1 pct_change * .5 * sym2_close + sym2_close
+            
+            payload = {
+            "type": "market",
+            "time_in_force": "day",
+            "take_profit": { "limit_price": f'{sym1_pct_change * .5 * list[1][1] + list[1][1]}'},
+            "symbol": f'{list[1][2]}',
+            "notional": "100",
+            "side": "buy"
+            }
+        
+            response = requests.post(url, json=payload, headers=headers())
+        
+            print(response.text)
+        
+        if sym2_pct_change - sym1_pct_change > .4:
+            #buy mkt sym1
+            #set take profit @ sym2 pct_change * .5 * sym1_close + sym1_close
+            payload = {
+            "type": "market",
+            "time_in_force": "day",
+            "take_profit": { "limit_price": f'{sym2_pct_change * .5 * list[0][1] + list[0][1]}'},
+            "symbol": f'{list[0][2]}',
+            "notional": "100",
+            "side": "buy"
+            }
+        
+            response = requests.post(url, json=payload, headers=headers())
+        
+            print(response.text)
+        
+        print(sym1_pct_change, sym2_pct_change)
+                
