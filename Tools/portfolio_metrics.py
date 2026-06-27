f83d090f0876f07351f728e0c90552122f2386d2
Niko <nikojlarin22@gmail.com>
2026-06-26 21:47:20 -0400

    Update portfolio_metrics.py

--- Tools/portfolio_metrics.py
+++ Tools/portfolio_metrics.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import numpy as np
 
-BALANCE_PATH = "/Users/nikolarin/Downloads/Custodial Brokerage_XXXX300_Balances_20260626-141945.CSV"
-TRANSACTION_PATH = "/Users/nikolarin/Downloads/Custodial_Brokerage_XXX300_Transactions_20260626-132230.csv"
-
+BALANCE_PATH = "C:\Desktop\Custodial Brokerage_XXXX300_Balances_20260626-141945.CSV"
+TRANSACTION_PATH = "C:\Desktop\Custodial_Brokerage_XXX300_Transactions_20260626-132230.csv"
 def clean_transaction_data(data):
     df = pd.read_csv(data, index_col="Date")
 
     df = df.drop(columns=["Fees & Comm"])
     df = df.dropna(subset=["Quantity", "Price", "Amount"])
-
+    df["Amount"] = df["Amount"].str.replace("$", "", regex=False)
+    df["Amount"] = df["Amount"].str.replace("-", "", regex=False)
+    
     return df
 
 def clean_balance_data(data, start_date):
@@ -43,11 +44,43 @@ def get_sharpe(balance_data, risk_free_rate = .04):
     sharpe = round(float(annual_return - risk_free_rate) / annual_volatility, 2)
 
     return sharpe
-balance_data = clean_balance_data(BALANCE_PATH, "5/7/2026")
+def get_pnl(data):
+    start = data["Amount"].iloc[0]
+    end = data["Amount"].iloc[-2]
+    pct_change = round((start - end) / end * 100, 2)
 
-print(f'Your Sharpe Ratio is    :   {get_sharpe(balance_data)}\n'\
-      f'Dail|Mont|Ann Vol       :   {get_volatility(balance_data)}'
-      )
+    return [round(start - end), float(pct_change)]
+
+def get_transaction_volume(transaction_data):
+    transaction_volume = transaction_data["Amount"]
+    transaction_volume = transaction_volume.astype(float)
+    transaction_volume = transaction_volume.sum()
+
+    return transaction_volume
+
+
+balance_data = clean_balance_data(BALANCE_PATH, "5/7/2026")
+transaction_data = clean_transaction_data(TRANSACTION_PATH)
+
+
+
+reporting_period = f'{balance_data.index[-1]} ----> {balance_data.index[0]}'
+portfolio_value = balance_data["Amount"][0]
+portfolio_sharpe = get_sharpe(balance_data)
+portfolio_volatility = get_volatility(balance_data)
+profit_loss = get_pnl(balance_data)
+all_time_transaction_volume = round(get_transaction_volume(transaction_data))
+
+
+print(f'Reporting Period        :   {reporting_period}\n'
+      f'Portfolio Value         :   ${portfolio_value}\n'
+      f'Your P&L is             :   ${profit_loss[0]} | {profit_loss[1]}%\n'
+      f'All time trading volume :   ${all_time_transaction_volume}\n'
+      f'Your Sharpe Ratio is    :   {portfolio_sharpe}\n'
+      f'Dail|Mont|Ann Vol       :   {portfolio_volatility}\n'
+)
+    
+
 
 '''
 Total volume
