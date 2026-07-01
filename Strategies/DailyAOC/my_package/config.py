2fa8e0725a8aaddd5ec3ca07bf0e4ddf8fcfa3cb
Niko <nikojlarin22@gmail.com>
2026-07-01 14:05:00 -0400

    Add headers function for API requests

---
+++ Strategies/DailyAOC/my_package/config.py
@@ -0,0 +1,7 @@
+def headers():
+    return {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "APCA-API-KEY-ID": 'YOUR-API-KEY',
+        "APCA-API-SECRET-KEY": 'YOUR-SECRET-API-KEY',
+    }
